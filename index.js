7d0657671206c346bab05eb2c8684b4eff9e1264
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-08 20:53:55 +0300

    render content as ul list from aray

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-console.log(ulEl)
-// 2. Grab the unordered list and store it in a const variable called ulEl
+
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   console.log(myLeads);
 });
-
+// Render the leads in the unordered list using ulEl.textContent
 for (let i = 0; i < myLeads.length; i++) {
-  console.log(myLeads[i]);
+  ulEl.textContent += myLeads[i] + " ";
 }
