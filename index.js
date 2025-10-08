24f9815f2cd288fe0126066e0d47d020d0e9c06b
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-08 20:43:19 +0300

    log out all leads in an array with for loop

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
-let myLeads = [];
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 
 inputBtn.addEventListener("click", function () {
-  // Push the value from the inputEl into the myLeads array
-  // instead of the hard-coded "www.awesomeleads.com" value
-  // Google -> "get value from input field javascript"
-  myLeads.push(inputEl.value)
-  console.log(myLeads)
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
 });
 
+  // Log out the items in the myLeads array using a for loop
+for (let i = 0; i < myLeads.length; i++) {
+  console.log(myLeads[i]);
+}
