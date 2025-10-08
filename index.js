836c88ff372faa4efba20271010bd17b0fde383c
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-08 21:28:51 +0300

    render li elements after for loop only once

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   console.log(myLeads);
 });
+// 1. Create a variable, listItems, to hold all the HTML for the list items
+// Assign it to an empty string to begin with
 
-// Let's try a different method!
+let listItems = "";
 for (let i = 0; i < myLeads.length; i++) {
-  // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-  // create element
-  // set text content
-  // append to ul
-  const li = document.createElement("li");
-  li.textContent = myLeads[i];
-  ulEl.append(li);
+  // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
+  listItems += "<li>" + myLeads[i] + "</li>";
 }
+// 3. Render the listItems inside the unordered list using ulEl.innerHTML
+ulEl.innerHTML = listItems;
