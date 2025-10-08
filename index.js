c7891b7e865d0537630358b373781fb33fcd47b0
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-08 16:52:51 +0300

    add empty array and const variable with input

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// Refactor the code so that it uses .addEventListener()
-// when you click the SAVE INPUT button
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
 
-function saveLead() {
-    console.log("Button clicked!")
-}
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
 
-let inputBtn = document.getElementById("input-btn");
-inputBtn.addEventListener("click", function() {
+inputBtn.addEventListener("click", function () {
   console.log("Button clicked!");
-})
+});
