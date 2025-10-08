acb14941a4864ad6ad49583be9b0bca70c692cc6
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-08 20:39:37 +0300

    save input values into array

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
-// Push the value "www.awesomelead.com" to myArray when the input button is clicked
 
 inputBtn.addEventListener("click", function () {
-  myLeads.push("www.awesomelead.com");
+  // Push the value from the inputEl into the myLeads array
+  // instead of the hard-coded "www.awesomeleads.com" value
+  // Google -> "get value from input field javascript"
+  myLeads.push(inputEl.value)
+  console.log(myLeads)
 });
 
