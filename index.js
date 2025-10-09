35a9ee0865a10d1e2fa13f78dbbef1aacea0c73d
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-09 07:50:26 +0300

    replace li + a tag with template strings

--- index.js
+++ index.js
@@ -15,12 +15,16 @@ inputBtn.addEventListener("click", function () {
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    // Wrap the lead in an anchor tag (<a>) inside the <li>
-    // Can you make the link open in a new tab?
-    listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
-    console.log(listItems)
-  }
-  ulEl.innerHTML = listItems;
+    // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
+    listItems += `
+      <li>
+          <a target='_blank' href='${myLeads[i]}'>
+            ${myLeads[i]}
+          </a>
+      </li>
+    `;
+  };
+  ulEl.innerHTML = listItems;;
 }
 
 
