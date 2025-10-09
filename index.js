20799c1bb27efef3375540b4cf5799a83f3e3d8b
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-09 05:25:31 +0300

    add line to clear out input field

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-
-  // 2. Call the renderLeads() function
+  // Clear out the input field
+  inputEl.value = "";
   renderLeads();
 });
 
-// 1. Wrap the code below in a renderLeads() function
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
     listItems += "<li>" + myLeads[i] + "</li>";
   }
   ulEl.innerHTML = listItems;
+
 }
+
