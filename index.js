5e670f6720e69e60633b54468b1b83cc195c491f
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-09 09:26:59 +0300

    learn and try localStorage feature

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+// 1. Save a key-value pair in localStorage
+// 2. Refresh the page. Get the value and log it to the console
+// 3. Clear localStorage
+
+// localStorage.setItem("myName", "Patrik Kusnir");
+let name = localStorage.getItem("myName");
+console.log(name)
+
+// localStorage.clear()
+// HINTS:
+// localStorage.setItem(key, value)
+// localStorage.getItem(key)
+// localStorage.clear()
+// PS: both key and value need to be strings
+
+
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
