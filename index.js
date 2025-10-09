627cc371f69843c71537ee5ade13f2cc12e64de5
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-09 19:15:24 +0300

    add save tab and save url from object in an array

--- index.js
+++ index.js
@@ -5,15 +5,23 @@ const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+// 1. Grab the SAVE TAB button and store it in a tabBtn variable
+const tabBtn = document.getElementById("tab-btn");
 
 if(leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
 
-// Refector the function so that it takes a parameter, leads, that it uses
-// instead of the global myLeads variable. Remember to update all invocations 
-// of the function as well.
+const tabs = [
+  {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+];
+
+// 2. Listen for clicks on tabBtn. Log Per's LinkedIn URL to the console
+tabBtn.addEventListener("click", function() {
+  console.log(tabs[0].url)
+})
+
 
 // less efficient way
 function render(leads) {
