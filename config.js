e3588ee8498091c8b8be1b8b8524688201e33f4d
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2026-02-14 19:01:30 +0300

    use firebase

---
+++ config.js
@@ -0,0 +1,5 @@
+export const config = {
+  databaseURL: "https://lead-s-tracker-app-a3bb8-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+
--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-database.js";
+import { config } from "./config.js"
+
+const firebaseConfig = {
+  databaseURL: config.databaseURL,
+  //"https://lead-s-tracker-app-a3bb8-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+console.log(firebaseConfig.databaseURL)
+
+
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
@@ -6,20 +21,19 @@ const deleteBtn = document.getElementById("delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 const tabBtn = document.getElementById("tab-btn");
 
-if(leadsFromLocalStorage) {
+if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
 
-tabBtn.addEventListener("click", function() {
+tabBtn.addEventListener("click", function () {
   // Grab the URL of the current tab!
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     myLeads.push(tabs[0].url);
     localStorage.setItem("myLeads", JSON.stringify(myLeads)); // turn array into the string
     render(myLeads);
-  })
-})
-
+  });
+});
 
 // less efficient way
 function render(leads) {
@@ -36,26 +50,21 @@ function render(leads) {
   ulEl.innerHTML = listItems;
 }
 
-deleteBtn.addEventListener("dblclick", function() {
+deleteBtn.addEventListener("dblclick", function () {
   localStorage.clear();
   myLeads = [];
   render(myLeads);
-})
+});
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   render(myLeads);
 });
-
-
-
 
 // alternative - more efficient way
 // function renderLead() {
 //   let listItem = "<li>" + inputEl.value + "</li>";
 //   ulEl.innerHTML += listItem;
 // }
-
-
